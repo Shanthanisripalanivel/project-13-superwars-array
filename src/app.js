ddddfc118d0b67eb7d0b07a4600f03bf878b949b
shanthanisripalanivel <shanthanisripalanivel@gmail.com>
2022-04-25 10:58:23 -0700

    fifth commit

--- src/app.js
+++ src/app.js
@@ -27,8 +27,16 @@ const initPlayers = (players) => {
 
     // Instead of forloop use Map method
     // Code here
-
+    detailedPlayers=players.map((player,i) => {
+        return {
+        name:player,
+        strength:getRandomStrength(),
+        image:"images/super-"+(i+1)+".png",
+        type:(i%2==0)? 'hero' : 'villain',
+        }
+    });  
     return detailedPlayers;
+
 }
 
 // getting random strength
@@ -43,9 +51,15 @@ const buildPlayers = (players, type) => {
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-
+    fragment=players.filter(player => player.type== type).map(player => 
+        `<div class="player">
+        <img src="${player.image}" alt="">
+        <div class="name">${player.name}</div>
+        <div class="strength">${player.strength}</div>
+        </div>`).join();
     return fragment;
 }
+
 
 // Display players in HTML
 const viewPlayers = (players) => {
